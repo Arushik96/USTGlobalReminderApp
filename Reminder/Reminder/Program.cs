2528778310bb06c0cd3d80540fab2eeb4a47923f
Arushik96 <37518813+Arushik96@users.noreply.github.com>
2018-03-19 04:04:53 +0530

    Add files via upload

---
+++ Reminder/Reminder/Program.cs
@@ -0,0 +1,115 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+namespace Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string connectionString = "SERVER=  localhost;" + "DATABASE=arushi;" + "UID=root;" + "PASSWORD=;"; //Set your MySQL connection string here.
+            string query = "select * from reminders;"; // set query to fetch data "Select * from  tabelname"; 
+
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            DateTime time = new DateTime();
+            string message = "";
+            List<string> reminderTime = new List<string>();
+            string rTime = "";
+            int flag = 0;
+            Program p = new Program();
+            
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            ds = new DataSet();
+                            adapter.Fill(ds);
+
+                            dt = ds.Tables[0];
+
+                            foreach (DataRow dr in dt.Rows)
+                            {
+                                time = Convert.ToDateTime(dr[2].ToString());
+                                if (time.Date.Equals(DateTime.Now.Date) || dr[5].ToString().Equals("yes"))
+                                {
+                                    if (!reminderTime.Contains(dr[2].ToString()))
+                                    {
+                                        reminderTime.Add(dr[2].ToString() + "*" + dr[1].ToString());
+                                    }
+                                }
+
+                            }
+                        }catch(Exception ex)
+                        {
+
+                        }
+                        foreach(string s in reminderTime)
+                        {
+                            try
+                            {
+
+                                message = s.Split('*')[1];
+                                rTime = s.Split('*')[0];
+                                if (Convert.ToDateTime(rTime).Hour.Equals(DateTime.Now.Hour) && Convert.ToDateTime(rTime).Minute.Equals(DateTime.Now.Minute))
+                                {
+                                    if (flag == 0)
+                                    {
+                                        p.showBalloon("ReminderApp", message);
+                                 
+                                        flag = 1;
+                                    }
+
+                                }
+                                else
+                                {
+                                    flag = 0;
+                                }
+                            }
+                            catch(Exception ex)
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
+        public void showBalloon(string title, string body)
+        {
+            notifyIcon.Visible = true;
+            notifyIcon.Icon = System.Drawing.SystemIcons.Information;
+            if (title != null)
+            {
+                notifyIcon.BalloonTipTitle = title;
+            }
+
+            if (body != null)
+            {
+                notifyIcon.BalloonTipText = body;
+            }
+
+            notifyIcon.ShowBalloonTip(30000);//30 seconds
+            Thread.Sleep(600000);
+            notifyIcon.Dispose();
+        }
+           
+        
+           
+        
+        
+    }
+}
---
+++ ReminderApp/Form1.cs
@@ -0,0 +1,232 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ReminderApp
+{
+    public partial class Form1 : Form
+    {
+        int ID = 0;
+        string connectionString = "SERVER=localhost;" + "DATABASE=arushi;" + "UID=root;" + "PASSWORD=;"; // Connection String to MySQL database
+        string path = @"...Reminder.exe"; //Path to Executable of Background Application
+        public Form1()
+        {
+            InitializeComponent();
+            Process[] pname = Process.GetProcessesByName("Reminder");// Checks the name of the process and starts it if not already being executed
+           if (pname.Length == 0)
+           {
+                Process.Start(path);
+           }
+            
+            
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.SetValue("ReminderApp", "\"" + path + "\""); // Sets the process in Start Up Directory of Windows 
+            }
+            groupBox2.Hide(); // Create New Reminder UI
+            groupBox3.Hide(); // Update Delete UI
+            dateTimePicker1.Format = DateTimePickerFormat.Time;
+            dateTimePicker2.Format = DateTimePickerFormat.Time;
+            dataGridView1.RowHeaderMouseClick += new DataGridViewCellMouseEventHandler(dataGridView1_RowHeaderMouseClick);
+            getGrid();
+        }
+        public void getGrid()
+        {
+            
+            string query = "select * from reminders;"; // set query to fetch data "Select * from  tabelname"; 
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                {
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    
+                    dataGridView1.DataSource = ds.Tables[0];
+                    if(ds.Tables[0].Rows.Count==0)
+                    {
+                    //    foreach (var process in Process.GetProcessesByName("Reminder"))
+                      //  {
+                        //    process.Kill();
+                        //}
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+           
+            groupBox1.Show();
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            groupBox2.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Set Reminder
+        {
+            string time = dateTimePicker1.Value.ToString();
+            string status = "active";
+            string message = textBox1.Text.ToString();
+            string snooze = "no";
+            string repeat = "no";
+            
+            if(checkBox2.Checked)
+            {
+                repeat = "yes";
+            }
+            try
+            {
+            
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                conn.Open();
+                MySqlCommand comm = conn.CreateCommand();
+                comm.CommandText = "INSERT INTO reminders(message,time,status,snooze,repeatDaily) VALUES('" + message + "','" + time + "','" + status + "','" + snooze + "','" + repeat + "')";
+                comm.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Reminder Set Succesfully");
+                groupBox2.Hide();
+                getGrid();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Something went Wrong! Please try again. ");
+                groupBox2.Hide();
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            dateTimePicker2.Value = Convert.ToDateTime(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString());
+            
+            if (dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString() == "yes")
+            {
+                checkBox3.Checked = true;
+            }
+            else
+            {
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            getGrid();
+            groupBox3.Show();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            groupBox3.Hide();
+        }
+        
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string time = dateTimePicker2.Value.ToString();
+            string status = "active";
+            string message = textBox2.Text.ToString();
+            string snooze = "no";
+            string repeat = "no";
+            
+            if (checkBox3.Checked)
+            {
+                repeat = "yes";
+            }
+            try
+            {
+            
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                conn.Open();
+                MySqlCommand comm = conn.CreateCommand();
+                comm.CommandText = "UPDATE reminders SET message='" + message + "',time='" + time + "',status='" + status + "',snooze='" + snooze + "',repeatDaily='" + repeat + "' where id="+ID+";";
+                comm.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Reminder Updated Succesfully");
+                groupBox3.Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went Wrong! Please try again. ");
+                
+            }
+        }
+        private void ClearData()
+        {
+
+            ID = 0;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+            if (ID != 0)
+            {
+                comm.CommandText = "DELETE from reminders where id=" + ID + ";";
+                comm.ExecuteNonQuery();
+                MessageBox.Show("Deleted Succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete Reminder");
+            }
+            conn.Close();
+            getGrid();
+
+            ClearData();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            MySqlCommand comm = conn.CreateCommand();
+             
+                comm.CommandText = "Truncate reminders;";
+                comm.ExecuteNonQuery();
+            
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.DeleteValue("ReminderApp");
+            }
+            MessageBox.Show("Deleted Succesfully");
+            
+            
+            conn.Close();
+            getGrid();
+
+            ClearData();
+        }
+    }
+}
